c3e511af298435caafc18f74b9bbe8754ecc0369
JKtimosha <79272266+JKtimosha@users.noreply.github.com>
2021-08-26 22:16:08 +0300

    bebra
    v

---
+++ cogs/moderation.py
@@ -0,0 +1,114 @@
+import discord
+import asyncio
+import json
+
+
+
+from datetime import datetime
+from discord.ext import commands
+from pymongo import MongoClient
+
+prefix = "f."
+class Main(commands.Cog):
+	def __init__(self,bot):
+		self.bot = bot
+		self.cooldown = []
+
+		self.cluster = MongoClient("mongodb+srv://SqOzY:149005060abs@cluster0.prjyw.mongodb.net/anticrush?retryWrites=true&w=majority")
+		self.db = self.cluster["warns"]
+		self.database = self.db["warns"]
+
+
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"Moderation is ready! ")
+
+
+	@commands.command(aliases = ["бан","забанить", "hammer","blacklist"])
+	@commands.has_permissions(ban_members=True)
+	async def ban(self, ctx, member:discord.User,*, reason = None):
+		if member == None or member == ctx.author or member == ctx.guild.owner: 
+			await ctx.send(embed = discord.Embed(title = "Неправильный заданный параметр",
+				description = "__Неправильный заданный параметр__\nПочему он не правильный? - \n__1. Вы не можете забанить самого себя\n2. Вы могли вообще не указать пользователя, которого хотите забанить!__\n3. Вы не МОжете забанить владельца сервера",
+				timestamp = datetime.utcnow(),
+				color  = discord.Colour.dark_green()))
+		elif type(member) == discord.Member:
+			await ctx.guild.kick(member, reason=reason, delete_message_days=0)
+			await ctx.send(embed = discord.Embed(title = member.display_name,
+				description = "Был успешно забанен по причине: {}".format(reason),
+				timestamp = datetime.utcnow(),
+				color = discord.Colour(0x9b59b6)))
+		else:
+			mem = discord.Object(member)
+			await ctx.guild.ban(
+                mem, reason=reason, delete_message_days=0
+            )
+			await ctx.send(embed = discord.Embed(title = mem,
+				description = "Был успешно забанен по причине: {}".format(reason),
+				timestamp = datetime.utcnow(),
+				color = discord.Colour(0x9b59b6)))
+
+
+	@commands.command(aliases = ["кикнуть", "пнуть", "кик", "выпнуть"])
+	@commands.has_permissions(kick_members = True)
+	async def kick(self, ctx, member:discord.User = None,*, reason = None):
+		if ctx.author.id == member.id or member.id == ctx.guild.owner_id:
+			pass
+		elif type(member) == discord.Member:
+			await ctx.guild.kick(member, reason=reason, delete_message_days=0)
+			await ctx.send(embed = discord.Embed(title = member.display_name,
+				description = "Был успешно кикнут по причине: {}".format(reason),
+				timestamp = datetime.utcnow(),
+				color = discord.Colour(0x9b59b6)))
+		else:
+			mem = discord.Object(member)
+			await ctx.guild.ban(
+                mem, reason=reason, delete_message_days=0
+            )
+			await ctx.send(embed = discord.Embed(title = mem,
+				description = "Был успешно кикнут по причине: {}".format(reason),
+				timestamp = datetime.utcnow(),
+				color = discord.Colour(0x9b59b6)))
+	@commands.command()
+	async def leaveban(ctx, *, reason = None):
+		await ctx.send(embed = discord.Embed(title ="Мод включен",description = "Теперь человек, который покинет сервер будет автоматически забанен",
+			timestamp = datetime.utcnow(),
+			color = discord.Colour(0x9b59b6)))
+		@commands.Cog.listener()
+		async def on_member_leave(member):
+			await member.ban(reason=reason)
+	@commands.Cog.listener()
+	async def on_guild_join(ctx):
+		for channel in ctx.guild.text_channels:
+			if channel.position == 1:
+				await ctx.send(embed = discord.Embed(title = "Хелп",
+					description = f"Спасибо, что добавили меня на свой сервер!\nМои небольшие параметры:\nПрефикс: {ctx.prefix}\nХелп команда: {ctx.prefix}help\nНаписан на: Python",
+					timestamp = datetime.utcnow(),
+					color = discord.Colour(0x9b59b6)))
+
+	@commands.command(aliases = ["очистить", "clearchat","очистить_чат"])
+	async def clear(self, ctx, limit:int = None):
+		if limit is None:
+			await ctx.send(embed = discord.Embed(title = "Ошибка..",
+				description = "Неверный синтаксис команды! Действуйте за этим припмером:\n> `f.очистить <число от 1 до 10000>`",
+				timestamp = datetime.utcnow(),
+				color = discord.Colour.dark_red()))
+		elif limit > 10000:
+			await ctx.channel.purge(limit = 10000)
+			await ctx.send(embed = discord.Embed(title = 'Успешно',
+				description = f"Очищено 10000 сообщений",
+				timestamp = datetime.utcnow(),
+				color = discord.Colour.blue()
+				))
+		else:
+			await ctx.channel.purge(limit=limit)
+			await ctx.send(embed = discord.Embed(title = "Успешно!",
+				description = f'Очищено {limit} сообщений!',
+				timestamp = datetime.utcnow(),
+				color = discord.Colour(0x9b59b6)))
+
+
+
+def setup(bot):
+	bot.add_cog(Main(bot))
---
+++ start.py
@@ -0,0 +1,81 @@
+import discord
+import os
+import sys
+from datetime import datetime
+from discord.ext import commands
+from Cybernator import Paginator
+import colorama
+colorama.init()
+os.system("cls")
+intents = discord.Intents.all()
+intents.members = True
+banner = f"""
+
+{colorama.Fore.RED}
+Logged as botuser 
+
+"""
+
+bot = commands.Bot(command_prefix = "f.", intents = intents)
+
+@bot.event
+async def on_ready():
+	await bot.change_presence(activity=discord.Activity(activity=discord.Streaming(
+        name=f"{len(bot.guilds)} guilds",url = "https://www.twitch.tv/monstercat")))
+
+
+
+
+			
+@bot.command()
+async def exit(ctx):
+	for role in ctx.guild.roles:
+		print(role.color)
+
+bot.remove_command("help")
+@bot.group(invoke_without_command = True)
+async def help(ctx, arg1 = None):
+	if arg1 is None:
+		embed111 = discord.Embed(title = "Помощь",
+			description = f"Префикс: `{ctx.prefix}`\nКатегории помощи:\n__1.__  `Фан`\n__2.__ `Модерация`\n__3.__ `логи`\n__4.__ `Саппорт`\n__5.__ `NSFW`",
+			timestamp = datetime.utcnow(),
+			color = discord.Colour(0x9b59b6))
+		embed = discord.Embed(title = "Модерация",
+			description = f"`{ctx.prefix}ban` - забанить участника\n`{ctx.prefix}kick` - забанить участника\n`{ctx.prefix}leaveban` - человек который покинет гильдию автоматически получит бан\n`{ctx.prefix}clear` - очистить определёное количество сообщений",
+			timestamp = datetime.utcnow(),
+			color = discord.Colour(0x9b59b6))
+		embed1 = discord.Embed(title = 
+			"фан",
+			description = f"`{ctx.prefix}биткоин` - показать курс биткоина\n`{ctx.prefix}время` - показать время\n`{ctx.prefix}рандом` - выбрать рандомное число\n`{ctx.prefix}пинг` - пинг бота",
+			timestamp = datetime.utcnow(),
+			color = discord.Colour(0x9b59b6)
+		)
+		embed2 = discord.Embed(title = "Саппорт",
+			description = f"`{ctx.prefix}report` - пожаловаться на краш бота\n`{ctx.prefix}support` - поддержать проект\n`{ctx.prefix}idea` - кинуть идею для бота",
+			color = discord.Colour(0x9b59b6))
+		embed3 = discord.Embed(title = "NSFW", description = f"`{ctx.prefix}lesbian` - лезбипорно\n`{ctx.prefix}blowjob` - минет\n`{ctx.prefix}cum` - suck\n`{ctx.prefix}anal` - анал\n`{ctx.prefix}tits` - сиськи",
+			color = discord.Colour(0x9b59b6))
+
+		list = [embed111, embed, embed1, embed2, embed3]
+		message = await ctx.send(embed=embed111)
+		page = Paginator(bot, message, only=ctx.author, use_more=False, embeds=list)
+		await page.start()
+
+
+@bot.event
+async def on_mention(ctx):
+	await ctx.send(embed = discord.Embed(title = None, 
+		description = f"Что? Мой префикс: {ctx.prefix}\nХелп команда - {ctx.prefix}",
+		timestamp = datetime.utcnow(),
+		color = discord.Colour(0x9b59b6)))
+for f in os.listdir("./cogs"):
+	if f.endswith(".py"):
+		bot.load_extension(f"cogs.{f[:-3]}")
+@bot.event
+async def on_command_error(ctx, error):
+	channel = bot.get_channel(863773962835984384)
+	await channel.send(embed = discord.Embed(description = f"{error}", color = discord.Colour.red()))
+try:
+	bot.run("")
+except KeyboardInterrupt:
+	sys.exit()
